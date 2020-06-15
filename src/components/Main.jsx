78355e93e7e5f6de37b70f80a219bc00c2ebfa6f
Kevin Chu <kevinchu.work@gmail.com>
2020-06-15 08:37:05 +0800

    Initial Menu bar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import Moment from 'react-moment';
+import randomString from 'random-string';
 import '../style/components/Main.scss';
 
 export default class Main extends React.Component {
 
 
   welcomeMsg = () => {
+    // let randomString = require('random-string');
+    let randStr = randomString({
+      length: 12,
+      numeric: true,
+      letters: false,
+      special: false
+    });
+
     return (
       <span className="welcomeMsg">
         <Moment format="MMM D YYYY HH:mm">{new Date()}</Moment>
+        <div>Reference number: {randStr}</div>
       </span>
     );
   }
